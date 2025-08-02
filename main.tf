454738683920314e00e6c180d9a7674fb02ee61a
mrathoredevopsengineer <148685072+mrathoredevopsengineer@users.noreply.github.com>
2025-08-02 15:18:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {}
 provider "azurerm" {
   features {}
-  subscription_id = "b30e900a-5ee7-4ea7-b027-4ea34f083e58"
+  subscription_id = "ba3dd1c9-2ade-4648-a13f-65581e2a2874"
 }
 
 resource "azurerm_resource_group" "gitconflict" {
